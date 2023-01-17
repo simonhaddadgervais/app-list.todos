5aee476cb96c1c95d36ad2dcbcd304c695cd8944
simonhaddadgervais <haddad.simon@aol.fr>
2023-01-17 10:29:30 +0100

    Adding ADD function

--- gui.py
+++ gui.py
@@ -2,12 +2,28 @@ import functions
 import PySimpleGUI as psg
 
 label = psg.Text("Type in a to-do")
-input_box = psg.InputText(tooltip="Enter to-do")
+input_box = psg.InputText(tooltip="Enter to-do", key="todo")
 add_button = psg.Button("Add")
 
 
-window = psg.PySimpleGUI.Window("To-Do App", layout=[[label], [input_box], [add_button]])
-window.read()
+window = psg.PySimpleGUI.Window("To-Do App",
+                                layout=[[label], [input_box], [add_button]],
+                                font=("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case psg.WINDOW_CLOSED:
+            break
+
+
 window.close()
 
 
