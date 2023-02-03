a1b2122d7abfc89d01637e0dce20fee059d6da0e
simonhaddadgervais <haddad.simon@aol.fr>
2023-02-03 16:12:15 +0100

    Design correction, adding gitignore, adding executable

--- gui.py
+++ gui.py
@@ -7,34 +7,29 @@ if not os.path.exists("todos.txt"):
     with open("todos.txt", "w") as file:
         pass
 
-psg.theme("TanBlue")
+psg.theme("DarkTeal10")
 clock = psg.Text('', key="clock", font=("Helvetica", 8))
 label = psg.Text("Type in a to-do")
 input_box = psg.InputText(tooltip="Enter to-do", key="todo")
-add_button = psg.Button("Add", button_color="Green",
-                        tooltip="Add to-do", key="Add")
+add_button = psg.Button("Add", tooltip="Add to-do", key="Add")
 list_box = psg.Listbox(values=functions.get_todos(),
                        key='todos', enable_events=True,
                        size=[40, 10])
 edit_button = psg.Button("Edit")
-complete_button = psg.Button("Complete", button_color="Green",
+complete_button = psg.Button("Complete",
                              tooltip="Complete", key="Complete")
-exit_button = psg.Button("Exit", button_color="DarkRed")
 
 window = psg.PySimpleGUI.Window("To-Do App",
                                 layout=[[clock],
                                         [label],
                                         [input_box, add_button],
-                                        [list_box, edit_button, complete_button],
-                                        [exit_button]],
+                                        [list_box, edit_button, complete_button]],
                                 font=("Helvetica", 15))
 
 while True:
     event, values = window.read(timeout=200)
     window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
-        case "Exit":
-            break
         case psg.WINDOW_CLOSED:
             break
         case "Add":
@@ -43,6 +38,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case "Edit":
             try:
                 todo_edit = values['todos'][0]
